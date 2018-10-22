3c9b1b962775ce1482dc53a389803f8a55bc66ae
Josh Hills <hillsj4@coventry.ac.uk>
2018-10-22 23:34:42 +0100

    added comments

--- Socket.py
+++ Socket.py
@@ -1,4 +1,16 @@
+'''
+Everything below this line is from this YouTube video: https://youtu.be/T8DLwACpe3o,
+modified slightly so that it works with the current version of Python.
+'''
+##################################################################################################
+'''
+The below functions are to do with IRC, which is a complex system in itself. With this script, we can
+add anything we want on top of it. This is just how the bot communicates with Twitch.
+'''
+##################################################################################################
+
 import socket
+
 from settings import HOST, PORT, PASS, IDENT, CHANNEL
 
 
--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
+'''
+Rather than having to go into the main Python script to change
+the name of the channel the bot is used on, it can instead be updated
+in this file under "CHANNEL".
+'''
+
 HOST = "irc.twitch.tv"
 PORT = 6667
-PASS = "oauth:6m4ytn5zeq2l5p4mnwmybfr903myjd"
-IDENT = "limitlessjosh"
-CHANNEL = "joshchatbottest"
+PASS = "oauth:6m4ytn5zeq2l5p4mnwmybfr903myjd" #oauth token from twitchapps.com for bot's account
+IDENT = "limitlessjosh" #bot's username
+CHANNEL = "joshchatbottest" #streamer's username
+
